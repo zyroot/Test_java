6dc8918f3a93af3abfbccd69a8c6f9fb121fd90b
zyroot <1209056907@qq.com>
2018-09-27 09:28:46 +0800

    ssh配置文件完成

---
+++ babaSport/src/main/java/cn/itcast/core/controller/CenterController.java
@@ -0,0 +1,16 @@
+package cn.itcast.core.controller;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * 后台管理
+ * 测试
+ * @author Zy
+ *
+ */
+@Controller
+public class CenterController {
+	public String name() {
+		return "";
+	}
+}
